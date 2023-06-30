ad6a7a26dc9770f64b6d5db3541f1e30f0029606
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2023-06-30 14:07:53 -0600

    Add files via upload

---
+++ files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+directory="PCAPS"
+subdirectories=(
+    'External_Pcaps'
+    'Denial_of_Service'
+    'Port_Scans'
+    'Layer_2_Attacks'
+)
+general_capture='.general.pcap'
+id_DoS=1
+file="DoS-${id_DoS}.pcap"
