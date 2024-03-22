d14bbd383aeb5a579d8d09b2824f58f418db3ef9
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2024-03-22 17:15:54 -0600

    Update Files.sh

--- Files.sh
+++ Files.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
-directory="PCAPS"
+pcaps_dir="PCAPS"
+logs_dir="LOGS"
+
+out_logs=(
+    'sessions.log'
+    'http.log'
+    'ssl.log'
+    'ssh.log'
+    'ftp.log'
+    'smtp.log'
+    'tftp.log'
+    'dns.log'
+    'smb2.log'
+    'dcerpc.log'
+    'ntlm.log'
+    'kerberos.log'
+    'llmnr.log'
+    'dhcp.log'
+    'dhcpv6.log'
+)
+
+in_logs=(
+    'sessions.log'
+    'http.log'
+    'ssl.log'
+    'ftp.log'
+    'smtp.log'
+    'tftp.log'
+    'dns.log'
+)
+
 subdirectories=(
     'Denial_of_Service'
     'Web_Attacks'
@@ -8,6 +38,7 @@ subdirectories=(
     'DNS_Tunneling'
     'Layer_2_Attacks'
 )
+
 general_capture=".general.pcap"
-id_pcap=0
-file="capture-${id_pcap}.pcap"
+whitelist_file_4="./whitelist4.txt"
+whitelist_file_6="./whitelist6.txt"
