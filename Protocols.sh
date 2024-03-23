02f0d9c3c87fc07061eddfdf7cfd2d316b07f046
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2024-03-22 21:34:58 -0600

    Create Protocols.sh

---
+++ Protocols.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+# +------------------------------------------------------------------------+
+# | TTP Tracker                                                            |
+# | -----------                                                            |
+# |                                                                        |
+# | Threat hunting endpoint tool in search of TTP in the network traffic.  |
+# |                                                                        |
+# | Author: Luis Herrera - @Ferhs343                                       |
+# | Contact: fer.hs343@gmail.com                                           |
+# | V 1.0.0                                                                |
+# |                                                                        |
+# +------------------------------------------------------------------------+
+
+function http() {
+
+    method=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.request.method" -T fields -e "http.request.method" 2> /dev/null)
+    uri=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.request.uri" -T fields -e "http.request.uri" 2> /dev/null)
+    user_agent=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.user_agent" -T fields -e "http.user_agent" 2> /dev/null)
+    mime_type=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.content_type" -T fields -e "http.content_type" 2> /dev/null)
+    status_code=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.response.code" -T fields -e "http.response.code" 2> /dev/null)
+}
+
+function dns() {
+
+    query=$(tshark -r "${general_capture}" -Y "udp.port == ${1} && dns.qry.name" -T fields -e "dns.qry.name" 2> /dev/null)
+    response_t=$(tshark -r "${general_capture}" -Y "udp.port == ${1} && dns.resp.type" -T fields -e "dns.resp.type" 2> /dev/null)
+    response_n=$(tshark -r "${general_capture}" -Y "udp.port == ${1} && dns.resp.name" -T fields -e "dns.resp.name" 2> /dev/null)
+}
