6b13066cc82c45f0268ee8537e68278719f61543
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2024-03-24 20:06:40 -0600

    Create Hunter.sh

---
+++ Hunter.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# +------------------------------------------------------------------------+
+# | TTP Tracker                                                            |
+# | -----------                                                            |
+# |                                                                        |
+# | Threat hunting endpoint tool in search of TTP in the network traffic.  |
+# |                                                                        |
+# | Author: Luis Herrera - @Ferhs343                                       |
+# | Contact: fer.hs343@gmail.com                                           |
+# | V 1.0.0                                                                |
+# |                                                                        |
+# +------------------------------------------------------------------------+
