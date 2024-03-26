9544956af0c157ad67fc31bacd905bd048903bd1
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2024-03-26 15:39:27 -0600

    Update Files.sh

--- Files.sh
+++ Files.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# +------------------------------------------------------------------------+
+# | TTP Tracker                                                            |
+# | -----------                                                            |
+# |                                                                        |
+# | Threat hunting endpoint tool in search of TTP in the network traffic.  |
+# |                                                                        |
+# | Author: Luis Herrera - @Ferhs343                                       |
+# | Contact: fer.hs343@gmail.com                                           |
+# | V 1.0.0                                                                |
+# |                                                                        |
+# +------------------------------------------------------------------------+
+
 pcaps_dir="PCAPS"
 logs_dir="LOGS"
 
