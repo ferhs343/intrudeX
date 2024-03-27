03385e1b96ded532645f42382c1bb0334a29e815
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2024-03-26 21:31:46 -0600

    Update Protocols.sh

--- Protocols.sh
+++ Protocols.sh
@@ -1,4 +1,3 @@
-
 #/bin/bash
 
 # +------------------------------------------------------------------------+
@@ -13,45 +12,22 @@
 # |                                                                        |
 # +------------------------------------------------------------------------+
 
-source Print_logs.sh
-
 function http() {
 
-    uri=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.request.uri" -T fields -e "http.request.uri" 2> /dev/null)
-    uri=($uri)
-
-    for (( l=0;l<="$(( ${#uri[@]} - 1 ))";l++ ));
-    do
-        method=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.request.uri == \"${uri[$l]}\"" -T fields -e "http.request.method" 2> /dev/null)
-	method+=("$method")
-        hostname=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.request.uri == \"${uri[$l]}\"" -T fields -e "http.host" 2> /dev/null)
-	hostname+=("$hostname")
-        user_agent=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.request.uri == \"${uri[$l]}\"" -T fields -e "http.user_agent" 2> /dev/null)
-	user_agent+=("$user_agent")
-        mime_type=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.request.uri == \"${uri[$l]}\"" -T fields -e "http.content_type" 2> /dev/null)
-	mime_type+=("$mime_type")
-        status_code=$(tshark -r "${general_capture}" -Y "tcp.port == ${1} && http.request.uri == \"${uri[$l]}\"" -T fields -e "http.response.code" 2> /dev/null)
-	status_code+=("$status_code")
-
-	data=("${method[$l]}" "${uri[$l]}" "${hostname[$l]}" "${user_agent[$l]}" "${mime_type[$l]}" "${status_code[$l]}")
-        preparing_log "${data[@]}"
-	print_log "${2}"
-    done
+    uri=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.request.uri" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    method=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.request.method" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    hostname=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.host" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    user_agent=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.user_agent" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    mime_type_rq=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.content_type" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    mime_type_rp=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.response" -T fields -e "http.content_type" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    status_code=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.response" -T fields -e "http.response.code" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
 }
 
 function dns() {
 
-    query=$(tshark -r "${general_capture}" -Y "udp.port == ${1} && dns.flags == 0x0100" -T fields -e "dns.qry.name" 2> /dev/null)
-    query=($query)
-
-    for (( l=0;l<="$(( ${#query[@]} - 1 ))";l++ ));
-    do
-       r_a=$(tshark -r "${general_capture}" -Y "udp.port == ${1} && dns.flags == 0x8180" -T fields -e "dns.a" 2> /dev/null)
-       r_aaaa=$(tshark -r "${general_capture}" -Y "udp.port == ${1} && dns.flags == 0x8180" -T fields -e "dns.aaaa" 2> /dev/null)
-       r_txt=$(tshark -r "${general_capture}" -Y "udp.port == ${1} && dns.flags == 0x8180" -T fields -e "dns.txt" 2> /dev/null)
-
-       data=("${query[$l]}" "${r_a}" "${r_aaaa}" "${r_txt}")
-       preparing_log "${data[@]}"
-       print_log "${2}"
-    done
+    query=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags == 0x0100" -T fields -e "dns.qry.name" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    r_a=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags == 0x8100" -T fields -e "dns.a" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    r_aaaa=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags == 0x8100" -T fields -e "dns.aaaa" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    r_txt=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags == 0x8100" -T fields -e "dns.txt" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    c_name=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags == 0x8100" -T fields -e "dns.cname" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
 }
