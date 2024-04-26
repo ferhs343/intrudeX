dd28812b522d607bc608e150558a3b07c57fbf9b
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2024-04-25 18:47:50 -0600

    Update Colors.sh

--- Colors.sh
+++ Colors.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 default="\033[0m\e[0m"
-yellow="\e[1;33m"
-red="\e[1;31m"
+yellow="\e[0;33m\033[1m"
+red="\e[0;31m\033[1m"
 cyan="\e[0;36m"
 green="\033[92m"
 purple="\e[1;95m"
-
-
-
