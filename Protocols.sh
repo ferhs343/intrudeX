2c84c7ea0a356735c8f1cc8872e3627508befcaa
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2024-04-28 22:18:53 -0600

    Update Protocols.sh

--- Protocols.sh
+++ Protocols.sh
@@ -16,17 +16,17 @@ source Files.sh
 
 function tcp() {
 
-    src_mac=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && ${2}" -T fields -e "eth.src" 2> /dev/null | sort -u)
-    src_ip=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && ${2}" -T fields -e "${ip_filter}.src" 2> /dev/null | sort -u)
-    src_port=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && ${2}" -T fields -e "tcp.srcport" 2> /dev/null | sort -u)
-    dst_mac=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && ${2}" -T fields -e "eth.dst" 2> /dev/null | sort -u)
-    dst_ip=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && ${2}" -T fields -e "${ip_filter}.dst" 2> /dev/null | sort -u)
-    dst_port=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && ${2}" -T fields -e "tcp.dstport" 2> /dev/null | sort -u)
+    src_mac=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "eth.src" 2> /dev/null | sort -u)
+    src_ip=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "${ip_filter}.src" 2> /dev/null | sort -u)
+    src_port=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "tcp.srcport" 2> /dev/null | sort -u)
+    dst_mac=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "eth.dst" 2> /dev/null | sort -u)
+    dst_ip=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "${ip_filter}.dst" 2> /dev/null | sort -u)
+    dst_port=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "tcp.dstport" 2> /dev/null | sort -u)
 }
 
 function fast_tcp() {
 
-    tshark -r "${general_capture}" -Y "(tcp.stream >= ${1} && tcp.stream <= ${2})" \
+    tshark -r "${general_capture}" \
     -T fields \
     -e "tcp.stream" \
     -e "frame.time" \
@@ -36,22 +36,22 @@ function fast_tcp() {
     -e "eth.dst" \
     -e "ip.dst" \
     -e "tcp.dstport" \
-    -e "tcp.flags" &> /dev/null > $3
+    -e "tcp.flags" &> /dev/null > $1
 }
 
 function udp() {
 
-    src_mac=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && ${2}" -T fields -e "eth.src" 2> /dev/null | sort -u)
-    src_ip=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && ${2}" -T fields -e "${ip_filter}.src" 2> /dev/null | sort -u)
-    src_port=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && ${2}" -T fields -e "udp.srcport" 2> /dev/null | sort -u)
-    dst_mac=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && ${2}" -T fields -e "eth.dst" 2> /dev/null | sort -u)
-    dst_ip=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && ${2}" -T fields -e "${ip_filter}.dst" 2> /dev/null | sort -u)
-    dst_port=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && ${2}" -T fields -e "udp.dstport" 2> /dev/null | sort -u)
+    src_mac=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "eth.src" 2> /dev/null | sort -u)
+    src_ip=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "${ip_filter}.src" 2> /dev/null | sort -u)
+    src_port=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "udp.srcport" 2> /dev/null | sort -u)
+    dst_mac=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "eth.dst" 2> /dev/null | sort -u)
+    dst_ip=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "${ip_filter}.dst" 2> /dev/null | sort -u)
+    dst_port=$(tshark -r "${stream_capture}" -Y "${1}" -T fields -e "udp.dstport" 2> /dev/null | sort -u)
 }
 
 function fast_udp() {
 
-    tshark -r "${general_capture}" -Y "(udp.stream >= ${1} && udp.stream <= ${2})" \
+    tshark -r "${general_capture}" \
     -T fields \
     -e "udp.stream" \
     -e "frame.time" \
@@ -60,25 +60,25 @@ function fast_udp() {
     -e "udp.srcport" \
     -e "eth.dst" \
     -e "ip.dst" \
-    -e "udp.dstport" &> /dev/null > $3
+    -e "udp.dstport" &> /dev/null > $1
 }
 
 function http() {
 
-    uri=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.request.uri" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    method=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.request.method" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    hostname=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.host" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    user_agent=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.user_agent" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    mime_type_rq=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.request" -T fields -e "http.content_type" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    mime_type_rp=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.response" -T fields -e "http.content_type" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    status_code=$(tshark -r "${general_capture}" -Y "tcp.stream eq ${1} && http.response" -T fields -e "http.response.code" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    uri=$(tshark -r "${stream_capture}" -Y "http.request" -T fields -e "http.request.uri" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    method=$(tshark -r "${stream_capture}" -Y "http.request" -T fields -e "http.request.method" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    hostname=$(tshark -r "${stream_capture}" -Y "http.request" -T fields -e "http.host" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    user_agent=$(tshark -r "${stream_capture}" -Y "http.request" -T fields -e "http.user_agent" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    mime_type_rq=$(tshark -r "${stream_capture}" -Y "http.request" -T fields -e "http.content_type" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    mime_type_rp=$(tshark -r "${stream_capture}" -Y "http.response" -T fields -e "http.content_type" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    status_code=$(tshark -r "${stream_capture}" -Y "http.response" -T fields -e "http.response.code" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
 }
 
 function dns() {
 
-    query=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags == 0x0100" -T fields -e "dns.qry.name" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    r_a=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags.response == 1" -T fields -e "dns.a" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    r_aaaa=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags.response == 1" -T fields -e "dns.aaaa" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    r_txt=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags.response == 1" -T fields -e "dns.txt" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
-    c_name=$(tshark -r "${general_capture}" -Y "udp.stream eq ${1} && dns.flags.response == 1" -T fields -e "dns.cname" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    query=$(tshark -r "${stream_capture}" -Y "dns.flags == 0x0100" -T fields -e "dns.qry.name" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    r_a=$(tshark -r "${stream_capture}" -Y "dns.flags.response == 1" -T fields -e "dns.a" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    r_aaaa=$(tshark -r "${stream_capture}" -Y "dns.flags.response == 1" -T fields -e "dns.aaaa" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    r_txt=$(tshark -r "${stream_capture}" -Y "dns.flags.response == 1" -T fields -e "dns.txt" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
+    c_name=$(tshark -r "${stream_capture}" -Y "dns.flags.response == 1" -T fields -e "dns.cname" 2> /dev/null | sed 's/^$/-/g' | tr -d ' ')
 }
